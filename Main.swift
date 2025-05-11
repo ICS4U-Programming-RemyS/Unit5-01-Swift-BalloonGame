4c0c610d56b326c9cbce88f6b7c4503464ad16a9
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-05-11 07:18:25 +0000

    Done Balloon game swift

--- Main.swift
+++ Main.swift
@@ -19,28 +19,28 @@ enum BalloonColor: String {
 // This is the Balloon class for the Balloon Game
 class Balloon {
     // Private variables to store position, color, ID, and pop state
-    private var xPosition: Int
-    private var yPosition: Int
+    private var balloonXPosition: Int
+    private var balloonYPosition: Int
     private var balloonColor: BalloonColor
-    private let id: Int
-    private var isPopped: Bool = false
+    private let balloonID: Int
+    private var isBalloonPopped: Bool = false
 
     // Constructor to initialize the balloon's attributes
-    init(x: Int, y: Int, color: BalloonColor, id: Int) {
-        self.xPosition = x
-        self.yPosition = y
+    init(xPosition: Int, yPosition: Int, color: BalloonColor, id: Int) {
+        self.balloonXPosition = xPosition
+        self.balloonYPosition = yPosition
         self.balloonColor = color
-        self.id = id
+        self.balloonID = id
     }
 
-    // Getter for x position
+    // Getter for x position of the balloon
     func getXPosition() -> Int {
-        return xPosition
+        return balloonXPosition
     }
 
-    // Getter for y position
+    // Getter for y position of the balloon
     func getYPosition() -> Int {
-        return yPosition
+        return balloonYPosition
     }
 
     // Getter for balloon color (as a string)
@@ -49,104 +49,104 @@ class Balloon {
     }
 
     // Getter for balloon ID
-    func getId() -> Int {
-        return id
+    func getBalloonID() -> Int {
+        return balloonID
     }
 
-    // Getter to check if balloon is popped
-    func getIsPopped() -> Bool {
-        return isPopped
+    // Getter to check if the balloon is popped
+    func isBalloonPopped() -> Bool {
+        return isBalloonPopped
     }
 
     // Method to move the balloon by a certain number of spaces
-    func moveBalloon(xSpaces: Int, ySpaces: Int) {
-        xPosition += xSpaces
-        yPosition += ySpaces
+    func moveBalloonHorizontally(by xSpaces: Int, vertically ySpaces: Int) {
+        balloonXPosition += xSpaces
+        balloonYPosition += ySpaces
     }
 
     // Method to change the balloon's color
-    func changeColor(newColor: BalloonColor) {
+    func changeBalloonColor(to newColor: BalloonColor) {
         balloonColor = newColor
     }
 
     // Method to pop the balloon
-    func pop() {
-        isPopped = true
-        print("Balloon \(id) has popped!")
+    func popBalloon() {
+        isBalloonPopped = true
+        print("Balloon \(balloonID) has popped!")
     }
 }
 
 // This is the Spike class for the Balloon Game
 class Spike {
     // Variable to store spike's x position
-    private var xPosition: Int
+    private var spikeXPosition: Int
     // Constant y position for all spikes (ground level)
-    static let Y_POSITION = 0
+    static let groundYPosition = 0
 
     // Constructor to set the initial x position of the spike
-    init(x: Int) {
-        self.xPosition = x
+    init(xPosition: Int) {
+        self.spikeXPosition = xPosition
     }
 
     // Method to move the spike horizontally
-    func moveSpikeHorizontally(xSpaces: Int) {
-        xPosition += xSpaces
+    func moveSpikeHorizontally(by xSpaces: Int) {
+        spikeXPosition += xSpaces
     }
 
-    // Getter for x position
-    func getXPosition() -> Int {
-        return xPosition
+    // Getter for spike's x position
+    func getSpikeXPosition() -> Int {
+        return spikeXPosition
     }
 
     // Method to check if a balloon is in line with the spike and on the ground, then pop it
-    func popBalloon(_ balloon: Balloon) {
-        if balloon.getXPosition() == xPosition &&
-            balloon.getYPosition() == Spike.Y_POSITION &&
-            !balloon.getIsPopped() {
-            balloon.pop()
+    func attemptToPopBalloon(_ balloon: Balloon) {
+        if balloon.getXPosition() == spikeXPosition &&
+            balloon.getYPosition() == Spike.groundYPosition &&
+            !balloon.isBalloonPopped() {
+            balloon.popBalloon()
         } else {
-            print("Balloon \(balloon.getId()) is safe!")
+            print("Balloon \(balloon.getBalloonID()) is safe!")
         }
     }
 }
 
 // Main game logic starts here
 
-// Create balloonA and balloonB with starting positions, colors, and IDs
-let balloonA = Balloon(x: 55, y: 41, color: .blue, id: 1)
-let balloonB = Balloon(x: 25, y: 85, color: .pink, id: 2)
+// Create two balloons with starting positions, colors, and IDs
+let firstBalloon = Balloon(xPosition: 55, yPosition: 41, color: .blue, id: 1)
+let secondBalloon = Balloon(xPosition: 25, yPosition: 85, color: .pink, id: 2)
 
-// Create a spike at x = 0
-let spike = Spike(x: 0)
+// Create a spike at x = 0 (ground level)
+let gameSpike = Spike(xPosition: 0)
 
-// Print initial positions and colors of balloons and spike
-print("Balloon A is at (\(balloonA.getXPosition()), \(balloonA.getYPosition())) and is \(balloonA.getBalloonColor())")
-print("Balloon B is at (\(balloonB.getXPosition()), \(balloonB.getYPosition())) and is \(balloonB.getBalloonColor())")
-print("Spike is at x = \(spike.getXPosition())\n")
+// Print initial positions and colors of the balloons and spike
+print("Balloon \(firstBalloon.getBalloonID()) is at (\(firstBalloon.getXPosition()), \(firstBalloon.getYPosition())) and is \(firstBalloon.getBalloonColor())")
+print("Balloon \(secondBalloon.getBalloonID()) is at (\(secondBalloon.getXPosition()), \(secondBalloon.getYPosition())) and is \(secondBalloon.getBalloonColor())")
+print("Spike is at x = \(gameSpike.getSpikeXPosition())\n")
 
 // Move balloons by a certain amount
-balloonA.moveBalloon(xSpaces: 34, ySpaces: 12)
-balloonB.moveBalloon(xSpaces: 43, ySpaces: 14)
+firstBalloon.moveBalloonHorizontally(by: 34, vertically: 12)
+secondBalloon.moveBalloonHorizontally(by: 43, vertically: 14)
 
 // Print positions after movement
-print("Balloon A is at (\(balloonA.getXPosition()), \(balloonA.getYPosition())) and is \(balloonA.getBalloonColor())")
-print("Balloon B is at (\(balloonB.getXPosition()), \(balloonB.getYPosition())) and is \(balloonB.getBalloonColor())")
-print("Spike is at x = \(spike.getXPosition())\n")
+print("Balloon \(firstBalloon.getBalloonID()) is now at (\(firstBalloon.getXPosition()), \(firstBalloon.getYPosition())) and is \(firstBalloon.getBalloonColor())")
+print("Balloon \(secondBalloon.getBalloonID()) is now at (\(secondBalloon.getXPosition()), \(secondBalloon.getYPosition())) and is \(secondBalloon.getBalloonColor())")
+print("Spike is at x = \(gameSpike.getSpikeXPosition())\n")
 
-// Move spike to the same x position as balloonA
-spike.moveSpikeHorizontally(xSpaces: 89)
+// Move the spike to the same x position as the first balloon
+gameSpike.moveSpikeHorizontally(by: 89)
 
 // Print new position of spike
-print("After moving spike to Balloon A:")
-print("Spike is at x = \(spike.getXPosition())\n")
+print("After moving spike to Balloon \(firstBalloon.getBalloonID()):")
+print("Spike is at x = \(gameSpike.getSpikeXPosition())\n")
 
-// Move balloonA vertically to the ground (y = 0)
-balloonA.moveBalloon(xSpaces: 0, ySpaces: -53)
+// Move first balloon vertically to the ground (y = 0)
+firstBalloon.moveBalloonHorizontally(by: 0, vertically: -53)
 
 // Print final position before popping attempt
-print("Balloon A is at (\(balloonA.getXPosition()), \(balloonA.getYPosition())) and is \(balloonA.getBalloonColor())")
-print("Spike is at x = \(spike.getXPosition())\n")
+print("Balloon \(firstBalloon.getBalloonID()) is now at (\(firstBalloon.getXPosition()), \(firstBalloon.getYPosition())) and is \(firstBalloon.getBalloonColor())")
+print("Spike is at x = \(gameSpike.getSpikeXPosition())\n")
 
-// Attempt to pop balloonA and balloonB
-spike.popBalloon(balloonA)
-spike.popBalloon(balloonB)
+// Attempt to pop first and second balloons
+gameSpike.attemptToPopBalloon(firstBalloon)
+gameSpike.attemptToPopBalloon(secondBalloon)
